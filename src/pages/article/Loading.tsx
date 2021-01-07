554097cb520b6c93d646d3dbac09434c24b500bc
편도걸 <pdg2491@naver.com>
2021-01-08 02:12:08 +0900

    [feat] Loading 페이지 추가

--- src/pages/article/Loading.tsx
+++ src/pages/article/Loading.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const spin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const LoadingWrapper = styled.div`
+  top: 50vh;
+  left: 50%;
+  position: absolute;
+  border: 4px solid #f3f3f3; /* Light grey */
+  border-top: 4px solid #3498db; /* Blue */
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  animation: ${spin} 2s linear infinite;
+`;
 
 function Loading() {
   return (
-    <div>Loading...</div>
+    <LoadingWrapper />
   );
 }
 
