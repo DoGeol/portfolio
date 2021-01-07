f891cb3753a6a87f1553e121fa74ce627c552957
편도걸 <pdg2491@naver.com>
2021-01-08 01:46:45 +0900

    [fix] FileCard URL 접근 시 전역 selectedHeaderId State 초기값 추가

--- src/components/Contents/FileTab/FileCard.tsx
+++ src/components/Contents/FileTab/FileCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import useContents from '../useContents';
 
 const CloseBtn = styled.i`
@@ -49,7 +49,11 @@ function FileCard(props: FileCardProps) {
   const { id, fileType, name, url, faClass, color } = props.info;
   const { selectedHeaderId, onSelected } = useContents();
   const isSelect: boolean = id === selectedHeaderId;
-  let history = useHistory();
+  const location = useLocation();
+  const history = useHistory();
+  if (location.pathname === url) {
+    onSelected(id);
+  }
   let fileOnClick = () => {
     if (isSelect) return;
     onSelected(id);
