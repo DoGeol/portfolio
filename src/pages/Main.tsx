edc4ddcef40a01366c3787e89c353113f446c27a
편도걸 <pdg2491@naver.com>
2021-01-08 00:25:34 +0900

    [feat] pages > Main 라우팅 설정 추가
    * 라우팅 페이지 컴포넌트 추가
      - Not Found
      - About
      - Project
      - Skill

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,12 +3,16 @@ import GlobalStyles from '../styled/GlobalStyles';
 import styled from 'styled-components';
 import MainNav from '../components/MainNav/MainNav';
 import Footer from '../components/Footer/Footer';
-import Contents from '../components/Contents/Contents';
+import NotFound from './NotFound';
+import About from './About';
+import Skill from './Skill';
+import Project from './Project';
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from '../modules';
+import FileTab from '../components/Contents/FileTab/FileTab';
 
 const store = createStore(rootReducer);
 
@@ -20,6 +24,25 @@ const AppWrapper = styled.div`
   }
 `;
 
+const Contents = styled.section`
+  top: 0;
+  left: 50px;
+  right: 0;
+  width: 100%;
+  position: relative;
+  @media screen and (max-width: 768px) {
+    top: 50px;
+    left: 0;
+  }
+`;
+
+const Article = styled.div`
+  height: 2000px;
+  width: 100%;
+  background: #282c34;
+  box-sizing: border-box;
+`;
+
 export default function Main() {
   return (
     <BrowserRouter>
@@ -27,7 +50,19 @@ export default function Main() {
         <AppWrapper>
           <GlobalStyles />
           <MainNav />
-          <Contents />
+          <Contents>
+            <FileTab />
+            <Article>
+              <Switch>
+                <Route exact path={'/'}>ROOT</Route>
+                <Route path={'/project'} component={Project} />
+                <Route path={'/skill'} component={Skill} />
+                <Route path={'/about'} component={About} />
+                <Route path={'/notFound'} component={NotFound} />
+                <Redirect path='*' to={'/notFound'} />
+              </Switch>
+            </Article>
+          </Contents>
           <Footer />
         </AppWrapper>
       </Provider>
