8f9a8da8866423ca9f5ad680d065af0b9d10dfe7
편도걸 <pdg2491@naver.com>
2021-01-14 14:47:58 +0900

    [fix] Root 페이지 TypingText 컴포넌트 추가

--- src/pages/article/Root.tsx
+++ src/pages/article/Root.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ArticleContainer, ArticleMainTitle } from '../../styled/common/ArticleStyled';
 import styled from 'styled-components';
+import TypingText from '../../components/Common/TypingText';
 
 const ArticleContainerWrapper = styled(ArticleContainer)`
   height: calc(100vh - 80px);
@@ -9,7 +10,9 @@ const ArticleContainerWrapper = styled(ArticleContainer)`
 function Root() {
   return (
     <ArticleContainerWrapper align='center'>
-      <ArticleMainTitle textAlign='center'>Getting Started</ArticleMainTitle>
+      <ArticleMainTitle textAlign='center'>
+        <TypingText isTyping={true} mainText={'Getting Started...'} textColor={'whitesmoke'} />
+      </ArticleMainTitle>
       <p>PDG's 포트폴리오</p>
     </ArticleContainerWrapper>
   );
